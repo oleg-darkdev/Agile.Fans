00c1b38f32f01b6dc4b31143f84137aa6a6acdaf
DarkDev <darkdev_brand@riseup.net>
2023-05-23 15:15:44 +0300

    alfa v.

---
+++ src/lib/core/layout/entities/ui/FreeDownload.svelte
@@ -0,0 +1,28 @@
+<div class="px-5 pb-24 lg:mx-0">
+	<div
+		class=" shadow-long mx-auto flex w-full max-w-6xl flex-col items-center justify-center overflow-hidden rounded-xl bg-gray-50 px-6 py-16 lg:px-12 lg:py-12"
+	>
+		<div
+			class="flex w-full flex-col items-center justify-start space-y-8 text-center lg:flex-row lg:justify-between lg:space-y-0 lg:text-left"
+		>
+			<div class="lg:max-w-xl">
+				<h2
+					class="mx-auto max-w-5xl pb-4 text-3xl font-bold leading-none tracking-tighter text-neutral-600 sm:text-3xl"
+				>
+					You can get a set of educational boardgames and access to web applications
+					<span class="ml-4 font-bold">
+						<span class="text-blue-500"> FREE </span>
+					</span>
+				</h2>
+			</div>
+			<div class="flex justify-start lg:mt-2">
+				<a
+					href="#price"
+					class="block w-full transform items-center rounded-xl border-2 border-white bg-blue-600 px-10 py-3.5 text-center text-base font-medium text-white shadow-md transition duration-500 ease-in-out focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+				>
+					Read more
+				</a>
+			</div>
+		</div>
+	</div>
+</div>

c358cbbadfafa6d3a054cb7548e1af7d03330b58
DarkDev <darkdev_brand@riseup.net>
2023-05-23 15:17:42 +0300

    refactoring structure components and upd path

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,53 @@
 <script>
-	import {
-		Features,
-		Description,
-		Product,
-		Price,
-		Header,
-		Hero,
-		Footer,
-		Reviews,
-    Logos
-	} from '../lib/widgets';
+	// почистить файл импортов agile.fans
 
-	import { products } from '../lib/shared';
+	import { Features, Description, Price, Product, Hero, Logos } from '../lib/agile.fans/widgets';
+	// 		FreeDownload,
+
+	import { Contact, FAQ, Header, Footer, Reviews, Stats, Cookie } from '../lib/core/layout/widgets';
+
+	import { productsList } from '../lib/core/data';
+	import { convertObjToArray } from '../lib/core/utils';
+
+	let arrayOfProducts = convertObjToArray(productsList);
+
+	const productData = productsList.agileFans;
+
+	// console.log(productsList);
+	// порефакторить, оставить прямые импорты где это необходимо, или динамические
 </script>
 
-<Header />
+<Header {productData} />
 
-<Hero />
+<Hero {productData} />
 <Logos />
+
+<Cookie />
 
 <a name="about" />
 <Description />
+<!-- {arrayOfProducts}
+ -->
+
+ <Stats stats={productData.stats} />
 
 <a name="features" />
 <Features />
 
-{#each products as product}
-	<Product {product} />
-{/each}
+<!-- <Product {product} /> -->
+<!-- {#each products as product} -->
+<!-- <Product {product} />
+{/each} -->
+
 <a name="price" />
 
 <Price />
 <Reviews />
 
 <a name="faq" />
-<a name="contact" />
+<FAQ />
 
-<Footer />
+<a name="contact" />
+<Contact {productData} />
+
+<Footer {productData} {arrayOfProducts} />

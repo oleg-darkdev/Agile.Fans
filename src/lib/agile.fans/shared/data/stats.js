a88d762b71b5839b4624929871de68aec92f3686
DarkDev <darkdev_brand@riseup.net>
2023-05-23 15:16:40 +0300

    alfa v.

---
+++ src/lib/agile.fans/shared/data/stats.js
@@ -0,0 +1,21 @@
+const stats = [
+	{
+		title: 'Lorem impsum',
+		value: 1000
+	},
+  {
+    title: 'Lorem impsum',
+    value: '34K'
+  },
+  {
+    title: 'Lorem impsum',
+    value: 230
+  },
+  {
+    title: 'Lorem impsum',
+    value: 5493
+  },
+];
+
+
+export default stats;

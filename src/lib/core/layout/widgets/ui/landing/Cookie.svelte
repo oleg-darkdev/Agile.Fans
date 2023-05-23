66a142041d7493dc461eccf8fd743a0d37badb12
DarkDev <darkdev_brand@riseup.net>
2023-05-23 15:15:33 +0300

    alfa v,

---
+++ src/lib/core/layout/widgets/ui/landing/Cookie.svelte
@@ -0,0 +1,26 @@
+<section
+	class="fixed left-12 bottom-16 mx-auto max-w-md rounded-2xl border border-gray-200 bg-blue-500 p-4 "
+>
+	<h2 class="font-semibold text-white ">🍪 Cookie Notice</h2>
+
+	<p class="mt-4 text-sm text-gray-100 ">
+		We use cookies to ensure that we give you the best experience on our website. <a
+			href="#"
+			class="text-blue-500 hover:underline">Read cookies policies</a
+		>.
+	</p>
+
+	<div class="mt-4 flex shrink-0 items-center justify-between gap-x-4">
+		<button
+			class="text-xs text-gray-200 underline transition-colors duration-100 hover:text-white focus:outline-none "
+		>
+			Manage your preferences
+		</button>
+
+		<button
+			class=" rounded-lg bg-gray-800 px-4 py-2.5 text-xs font-medium text-white transition-colors duration-300 hover:bg-gray-700 focus:outline-none"
+		>
+			Accept
+		</button>
+	</div>
+</section>

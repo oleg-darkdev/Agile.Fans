21730679408df6bac2c468c636d9cb2e6ff7e7a7
DarkDev <darkdev_brand@riseup.net>
2023-05-27 19:39:08 +0300

    alfa v.

---
+++ src/lib/core/layout/entities/ui/ShopItemCard.svelte
@@ -0,0 +1,114 @@
+<script>
+  export let item;
+</script>
+
+<div
+	class="flex max-w-sm flex-col rounded-lg border border-gray-200 bg-white p-0 text-gray-500 shadow-md dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400"
+>
+	<a href="/">
+		<img class="rounded-t-lg p-8" src="/images/product-1.webp" alt="product 1" />
+	</a>
+	<div class="px-5 pb-5">
+		<a href="/">
+			<h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+				Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+			</h5>
+		</a>
+		<div class="mt-2.5 mb-5 flex items-center">
+			<svg width="18" height="18" aria-label="star" viewBox="100 100 120 120">
+				<defs>
+					<linearGradient id="li62ninq">
+						<stop offset="0%" stop-color="#F5CA14" />
+						<stop offset="100%" stop-color="#F5CA14" />
+					</linearGradient>
+				</defs>
+				<g fill="url(#li62ninq)" stroke="#F5CA14" stroke-width="2">
+					<polygon
+						points="165.000, 185.000, 188.511, 197.361, 184.021, 171.180,
+                           203.042, 152.639, 176.756, 148.820, 165.000, 125.000,
+                           153.244, 148.820, 126.958, 152.639, 145.979, 171.180,
+                           141.489, 197.361, 165.000, 185.000"
+					/>
+				</g>
+			</svg>
+			<svg width="18" height="18" aria-label="star" viewBox="100 100 120 120">
+				<defs>
+					<linearGradient id="li62ninq">
+						<stop offset="0%" stop-color="#F5CA14" />
+						<stop offset="100%" stop-color="#F5CA14" />
+					</linearGradient>
+				</defs>
+				<g fill="url(#li62ninq)" stroke="#F5CA14" stroke-width="2">
+					<polygon
+						points="165.000, 185.000, 188.511, 197.361, 184.021, 171.180,
+                           203.042, 152.639, 176.756, 148.820, 165.000, 125.000,
+                           153.244, 148.820, 126.958, 152.639, 145.979, 171.180,
+                           141.489, 197.361, 165.000, 185.000"
+					/>
+				</g>
+			</svg>
+			<svg width="18" height="18" aria-label="star" viewBox="100 100 120 120">
+				<defs>
+					<linearGradient id="li62ninq">
+						<stop offset="0%" stop-color="#F5CA14" />
+						<stop offset="100%" stop-color="#F5CA14" />
+					</linearGradient>
+				</defs>
+				<g fill="url(#li62ninq)" stroke="#F5CA14" stroke-width="2">
+					<polygon
+						points="165.000, 185.000, 188.511, 197.361, 184.021, 171.180,
+                           203.042, 152.639, 176.756, 148.820, 165.000, 125.000,
+                           153.244, 148.820, 126.958, 152.639, 145.979, 171.180,
+                           141.489, 197.361, 165.000, 185.000"
+					/>
+				</g>
+			</svg>
+			<svg width="18" height="18" aria-label="star" viewBox="100 100 120 120">
+				<defs>
+					<linearGradient id="li62ninq">
+						<stop offset="0%" stop-color="#F5CA14" />
+						<stop offset="100%" stop-color="#F5CA14" />
+					</linearGradient>
+				</defs>
+				<g fill="url(#li62ninq)" stroke="#F5CA14" stroke-width="2">
+					<polygon
+						points="165.000, 185.000, 188.511, 197.361, 184.021, 171.180,
+                           203.042, 152.639, 176.756, 148.820, 165.000, 125.000,
+                           153.244, 148.820, 126.958, 152.639, 145.979, 171.180,
+                           141.489, 197.361, 165.000, 185.000"
+					/>
+				</g>
+			</svg>
+			<svg width="18" height="18" aria-label="star" viewBox="100 100 120 120">
+				<defs>
+					<linearGradient id="li62ninr">
+						<stop offset="0%" stop-color="#F5CA14" />
+						<stop offset="0%" stop-color="#F5CA14" />
+						<stop offset="0%" stop-color="transparent" />
+						<stop offset="100%" stop-color="transparent" />
+					</linearGradient>
+				</defs>
+				<g fill="url(#li62ninr)" stroke="#F5CA14" stroke-width="2">
+					<polygon
+						points="165.000, 185.000, 188.511, 197.361, 184.021, 171.180,
+                           203.042, 152.639, 176.756, 148.820, 165.000, 125.000,
+                           153.244, 148.820, 126.958, 152.639, 145.979, 171.180,
+                           141.489, 197.361, 165.000, 185.000"
+					/>
+				</g>
+			</svg>
+			<span
+				class="ml-3 inline-flex items-center justify-center rounded bg-blue-100 px-2.5 py-0.5 text-xs font-medium text-blue-800 dark:bg-blue-900 dark:text-blue-300"
+				>4
+			</span>
+		</div>
+		<div class="flex items-center justify-between">
+			<span class="text-3xl font-bold text-gray-900 dark:text-white">$543 </span>
+			<a
+				href="/"
+				class="bg-primary-700 hover:bg-primary-800 dark:bg-primary-600 dark:hover:bg-primary-700 focus:ring-primary-300 dark:focus:ring-primary-800 inline-flex items-center justify-center rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
+				>Buy now
+			</a>
+		</div>
+	</div>
+</div>

172f1a9a3b255ae3f571565da5415ae36c1cd965
DarkDev <darkdev_brand@riseup.net>
2023-05-29 14:12:27 +0300

    add pseudo content

--- src/lib/core/data/index.js
+++ src/lib/core/data/index.js
@@ -1,8 +1,34 @@
-import { agileFans } from './products';
+import { agileFans, mui, agi, hui, yui, aui, qui } from './products';
 import social from './social';
 import contact from './contact';
 
 const productsList = {
 	agileFans,
+	mui,
+	agi,
+	hui,
+	yui,
+	aui,
+	qui
 };
-export { productsList, contact, social };
+
+const productsCategories = [
+	{
+		title: 'all',
+		products: [agileFans, mui, agi, hui, yui, aui, qui]
+	},
+	{
+		title: 'AgileKIT',
+		products: [agileFans, qui]
+	},
+	{
+		title: 'KanbanKIT',
+		products: [hui, yui, qui]
+	},
+	{
+		title: 'ScrumKIT',
+		products: [hui, yui, qui]
+	}
+];
+
+export { productsList, contact, social, productsCategories };

0f2030d8b53a74f3acc71826bfa7d55b1a78cb42
DarkDev <darkdev_brand@riseup.net>
2023-05-30 18:00:07 +0300

    add alfa v. login/signIn btns panel

--- src/lib/core/layout/entities/index.js
+++ src/lib/core/layout/entities/index.js
@@ -1,5 +1,6 @@
 import FreeDownload from './ui/FreeDownload.svelte';
 import ShopItemCard from './ui/ShopItemCard.svelte';
+import BtnLogIn from './ui/BtnLogIn.svelte';
+import BtnSignIn from './ui/BtnSignIn.svelte';
 
-
-export {  FreeDownload, ShopItemCard };
+export { FreeDownload, ShopItemCard, BtnLogIn, BtnSignIn };
---
+++ src/lib/core/layout/entities/ui/BtnLogIn.svelte
@@ -0,0 +1,4 @@
+<button
+	class="block transform items-center rounded-xl border-2 border-white px-10 py-2.5 text-center text-base font-medium text-white shadow-md transition duration-500 ease-in-out focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+	>Sign in</button
+>

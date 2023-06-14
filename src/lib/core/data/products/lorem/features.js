4e4ad20e9229b411fcf960a23511c38cb335d749
DarkDev <darkdev_brand@riseup.net>
2023-06-14 17:24:20 +0300

    init lorem data

---
+++ src/lib/core/data/products/lorem/features.js
@@ -0,0 +1,34 @@
+const features = [
+	{
+		icon: '.svg',
+		title: 'Lorem ipsum dolor',
+		desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	},
+	{
+		icon: '.svg',
+		title: 'Lorem ipsum dolor',
+		desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	},
+	{
+		icon: '.svg',
+		title: 'Lorem ipsum dolor',
+		desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	},
+	{
+		icon: '.svg',
+		title: 'Lorem ipsum dolor',
+		desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	},
+	{
+		icon: '.svg',
+		title: 'Lorem ipsum dolor',
+		desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	},
+	{
+		icon: '.svg',
+		title: 'Lorem ipsum dolor',
+		desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	}
+];
+
+export default features;

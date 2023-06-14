ac0906c2b5107399424f75b8a11dc95912c94871
DarkDev <darkdev_brand@riseup.net>
2023-06-14 17:32:37 +0300

    integrate the 'spyral dynamics' project

---
+++ src/routes/spiral-dynamics/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import {
+		SpyralTypesCardsSection,
+		Hero,
+		SpyralTypeSection,
+		NewsLetter,
+		TestimonialsSection
+	} from '../../lib/spyral-dynamics/landing/widgets';
+</script>
+
+
+<SpyralTypesCardsSection />
+<Hero />
+<SpyralTypeSection />
+<NewsLetter />
+<TestimonialsSection />

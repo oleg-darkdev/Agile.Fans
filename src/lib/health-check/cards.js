9e717f96362a5663d44904e80b9b0e6340f068fc
DarkDev <darkdev_brand@riseup.net>
2023-06-14 17:33:06 +0300

    alfa v.

---
+++ src/lib/health-check/cards.js
@@ -0,0 +1,72 @@
+const cards = [
+	{
+		title: 'Easy to release',
+		icon: 'release.svg',
+		id: 1,
+		positive: 'Releasing is simple, safe, painless & mostly automated',
+		negative: 'Releasing is risky, painful, lots of manual work, and takes forever',
+		status: ''
+	},
+	{
+		title: 'Suitable process',
+		icon: 'process.svg',
+		id: 2,
+		negative: 'Our way of working sucks',
+		positive: 'Our way of working fits us perfectly',
+		status: ''
+	},
+	{
+		title: 'Tech quality',
+		icon: 'tech.svg',
+		id: 3,
+		negative: 'Our code is a pile of dung, and technical debt is raging out of control',
+		positive:
+			'We’re proud of the quality of our code! It is clean, easy to read, and has great test coverage',
+		status: ''
+	},
+	{
+		title: 'Value',
+		icon: 'value.svg',
+		id: 4,
+		positive: 'We deliver great stuff! We’re proud of it and our stakeholders are really happy',
+		negative: 'We deliver crap. We feel ashamed to deliver it. Our stakeholders hate us',
+		status: ''
+	},
+	{
+		title: 'Speed',
+		icon: 'speed.svg',
+		id: 5,
+		positive:
+			'Stories keep getting stuck on dependencies We get stuff done really quickly. No waiting, no delays',
+		negative: 'We never seem to get done with anything. We keep getting stuck or interrupted.',
+		status: ''
+	},
+	{
+		title: 'Mission',
+		icon: 'mission.svg',
+		id: 6,
+		positive: 'We know exactly why we are here, and we are really excited about it',
+		negative:
+			'We have no idea why we are here, there is no high level picture or focus. Our so-called mission is completely unclear and uninspiring',
+		status: ''
+	},
+	{
+		title: 'Fun',
+		icon: 'fun.svg',
+		id: 7,
+		positive: 'We love going to work, and have great fun working together',
+		negative: 'Boooooooring',
+		status: ''
+	},
+	{
+		title: 'Learning',
+		icon: 'learning.svg',
+		id: 8,
+		positive: 'We’re learning lots of interesting stuff all the time',
+		negative: 'We never have time to learn anything',
+		status: ''
+	}
+];
+
+
+export default cards;
---
+++ src/routes/health-check/+page.svelte
@@ -0,0 +1,142 @@
+<script>
+	import { ShowFullListBtn } from '../../lib/core/layout/shared';
+	import { FeaturesForProductType } from '../../lib/core/layout/entities';
+	import { Features } from '../../lib/action-delegation/landing/widgets';
+  import cards from '../../lib/health-check/cards';
+
+	import {
+		Contact,
+		FreeDownload,
+		ProductVariants,
+		FaqProduct,
+		CallToWorkshop,
+		AboutProduct,
+		Reviews,
+		HeroProduct,
+		HeaderBottomLine,
+		Stats,
+		Cookie
+	} from '../../lib/core/layout/widgets';
+
+	$: showFullList = true;
+
+	import { productsList } from '../../lib/core/data';
+	const productData = productsList.healthCheck;
+
+
+	const steps = [
+		'Create your board',
+		'Share the unique link with your team',
+		'Evaluate the health of the team',
+		'Use the results as a conversation starter'
+	];
+</script>
+
+<!--
+Teoria stopniej delegowania
+
+Warsztaty
+Prices
+FAQ
+Contact -->
+
+<!--
+  ToDo:
+
+  В виде формы сделать - и данные передаются в таблицу?
+  https://teamhealthcheck.io/about
+ -->
+
+<HeaderBottomLine />
+
+<Cookie />
+
+<HeroProduct shortDesc={productData.shortDesc} />
+<!-- Health Check - How is your team feeling? -->
+<!-- A free, anonymous, and super simple tool to run a version of the Spotify Health Check Survey. -->
+
+<Features features={productData.features} />
+
+<!-- <Stats stats={productData.stats} /> -->
+<AboutProduct about={productData.about} title={productData.shortDesc.title}>
+	<h2 class="mb-2 mt-4 text-center text-xl font-semibold text-gray-800 sm:text-2xl md:mb-4 md:text-left">
+		Steps for starting
+	</h2>
+	<ol class="mb-4">
+		{#each steps as step, i}
+			<li class="text-center font-medium text-gray-500 md:text-left"><span class="font-medium text-gray-500">{i}.</span> {step}</li>
+		{/each}
+	</ol>
+</AboutProduct>
+
+<!--
+
+All answers are anonymous, and no login is required.
+
+You can create an account to organize the different polls from your team. Still, no login is required from your team. -->
+<section class="mb-12 flex flex-col items-center justify-center">
+	<div class="mx-auto grid max-w-5xl gap-5  lg:grid-cols-3">
+		{#each cards as card}
+			<div class="flex flex-col overflow-hidden rounded-lg shadow-lg">
+				<div class="flex-shrink-0">
+					<img class="h-48 w-full object-cover" src={card.icon} alt={card.title} />
+				</div>
+				<div class="flex flex-1 flex-col justify-between bg-white p-6">
+					<div class="flex-1">
+						<a href="#" class="mt-2 block">
+							<h4 class="text-xl font-semibold text-neutral-600">{card.title}</h4>
+							<div class="mt-3 ">
+								<div class="badge-success  badge-outline badge">positive</div>
+								<p class="text-base text-gray-500">
+									{card.positive}
+								</p>
+							</div>
+							<div class="mt-3 ">
+								<div class="badge-error badge-outline badge">negative</div>
+								<p class="text-base text-gray-500">
+									{card.negative}
+								</p>
+							</div>
+						</a>
+					</div>
+					<!-- <div class="mt-6 flex items-center">
+					<div class="flex-shrink-0">
+						<a href="https://twitter.com/Mike_Andreuzza">
+							<span class="sr-only">Michael Andreuzza</span>
+							<img
+								class="h-10 w-10 rounded-full"
+								src="https://d33wubrfki0l68.cloudfront.net/2f76102fd18a4e095eaed7a836a3f2183a982a4d/91dd4/images/avatar.jpg"
+								alt=""
+							/>
+						</a>
+					</div>
+					<div class="ml-3">
+						<p class="text-sm font-medium text-neutral-600">
+							<a href="https://twitter.com/Mike_Andreuzza" class="hover:underline">
+								Michaerl Andreuzza</a
+							>
+						</p>
+						<div class="flex space-x-1 text-sm text-gray-500">
+							<time datetime="2020-03-16"> Mar 16, 2020 </time>
+							<span aria-hidden="true"> · </span>
+							<span> 6 min read </span>
+						</div>
+					</div>
+				</div> -->
+				</div>
+			</div>
+		{/each}
+	</div>
+</section>
+
+<CallToWorkshop />
+
+<Reviews feedback={productData.feedback} />
+
+<ProductVariants>
+	<FeaturesForProductType />
+</ProductVariants>
+
+<FaqProduct faq={productData.faq} />
+
+<Contact />

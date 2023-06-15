a3f6708ca340259f0b7cdccd452969fb453c7cdf
DarkDev <darkdev_brand@riseup.net>
2023-06-15 13:45:47 +0300

    afla v. video section for sub products

---
+++ src/lib/core/layout/widgets/ui/landing/Video.svelte
@@ -0,0 +1,99 @@
+<script>
+	export let link;
+</script>
+
+<section class="my-24  mx-auto flex justify-center md:px-6 ">
+	<div class="flex max-w-7xl flex-wrap  rounded-lg bg-gray-200 py-14 px-12">
+		<div class="mb-12 w-full shrink-0 grow-0 basis-auto lg:mb-0 lg:-mr-[70px] lg:w-5/12">
+			<div class="flex lg:py-12">
+				<iframe
+					class="embed-responsive-item w-full h-[250px] z-10 rounded-lg shadow-lg"
+					src={link}
+					allowfullscreen=""
+					data-gtm-yt-inspected-2340190_699="true"
+					id="240632615"
+				/>
+			</div>
+		</div>
+		<a name="workshops" />
+
+		<div class=" w-full shrink-0 grow-0 basis-auto lg:w-7/12">
+			<div
+				class="flex h-full items-center rounded-lg bg-primary p-6 text-center text-white lg:pl-12 lg:text-left"
+			>
+				<div class="lg:pl-12">
+					<h2 class="mb-6 text-3xl font-bold">Pozostały pytania ?</h2>
+					<p class="mb-6 pb-2 lg:pb-0">
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, sint, repellat vel quo
+						quisquam accusamus in qui at ipsa enim quibusdam illo laboriosam omnis. Labore itaque
+						illum distinctio eum neque!
+					</p>
+					<a
+						href=""
+						class="rounded-lg border-2 border-neutral-50 bg-blue-600 px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-100 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200"
+						data-te-ripple-init
+						data-te-ripple-color="light"
+					>
+						Rejestracja na warsztaty
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--
+	<div class="px-6 py-12 text-center md:px-12 lg:text-left">
+		<div class="container mx-auto">
+			<div class="grid items-center gap-12 lg:grid-cols-2">
+				<div class="mt-12 lg:mt-0">
+					<h2
+						class="mb-12 text-5xl font-bold tracking-tight text-[hsl(218,81%,95%)] md:text-6xl xl:text-7xl"
+					>
+						Pozostały pytania ?
+					</h2>
+					<p class="text-lg text-[hsl(218,81%,95%)]">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima officia consequatur
+						adipisci tenetur repudiandae rerum quos.
+					</p>
+				</div>
+				<div class="mb-12 lg:mb-0">
+					<div
+						class="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden rounded-lg shadow-lg"
+						style="padding-top: 56.25%"
+					>
+						<iframe
+							class="embed-responsive-item absolute top-0 right-0 bottom-0 left-0 h-full w-full"
+							src={link}
+							allowfullscreen=""
+							data-gtm-yt-inspected-2340190_699="true"
+							id="240632615"
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section> -->
+
+<!-- <section class="background-radial-gradient container my-24 mx-auto mb-32 md:px-6"> -->
+<style>
+	.background-radial-gradient {
+		background-color: hsl(218, 41%, 15%);
+		background-image: radial-gradient(
+				650px circle at 0% 0%,
+				hsl(218, 41%, 35%) 15%,
+				hsl(218, 41%, 30%) 35%,
+				hsl(218, 41%, 20%) 75%,
+				hsl(218, 41%, 19%) 80%,
+				transparent 100%
+			),
+			radial-gradient(
+				1250px circle at 100% 100%,
+				hsl(218, 41%, 45%) 15%,
+				hsl(218, 41%, 30%) 35%,
+				hsl(218, 41%, 20%) 75%,
+				hsl(218, 41%, 19%) 80%,
+				transparent 100%
+			);
+	}
+</style>

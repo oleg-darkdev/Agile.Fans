4026775f814fd09a6975d051a864dac5848e8265
DarkDev <darkdev_brand@riseup.net>
2023-06-23 01:23:08 +0300

    add products table, refactoring header export

--- src/routes/demo/+page.svelte
+++ src/routes/demo/+page.svelte
@@ -1,14 +1,32 @@
 <script>
 	import { HeaderApp } from '../../lib/core/layout/widgets';
+	import { ProductItem } from '../../lib/core/layout/entities';
+
 	import { Aside } from '../../lib/core/layout/entities';
 
 	import { productsList } from '../../lib/core/data';
 	import { convertObjToArray } from '../../lib/core/utils';
 
-	const arrayOfProducts = convertObjToArray(productsList),
-		productData = productsList.agileKit;
+	const arrayOfProducts = convertObjToArray(productsList);
+
+	$: selectedApp = '';
 </script>
 
-<HeaderApp {productData} />
+<HeaderApp productData={productsList.agileKit} />
 
-<Aside />
+<main class="flex flex-row ">
+	<Aside {selectedApp} />
+
+	<div class="container my-24 mx-auto md:px-6">
+		<section class="mb-32 text-center">
+			<!-- <h2 class="mb-12 pb-4 text-center text-3xl font-bold">Latest articles</h2> -->
+
+			<div class="grid gap-6 lg:grid-cols-4 lg:gap-x-8 lg:gap-y-10">
+				{#each arrayOfProducts as product}
+					<ProductItem {product} />
+				{/each}
+			</div>
+		</section>
+	</div>
+</main>
+<!-- {#if selectedApp}{/if} -->

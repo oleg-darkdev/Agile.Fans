27900176404f9ef1aa31b7c99c5eea6310d1385a
DarkDev <darkdev_brand@riseup.net>
2023-06-26 17:10:00 +0300

    fix card data

---
+++ src/lib/core/data/kanbanKit/card.js
@@ -0,0 +1,55 @@
+const card = {
+	//   core
+	id: '636d4c81150e4204acfa795f',
+	// генериться с борды
+	shortLink: 'J2U7fJ7L',
+	shortUrl: 'https://trello.com/c/J2U7fJ7L',
+	url: 'https://trello.com/c/J2U7fJ7L/1-magdalena-jablonska',
+	//  на уровень борды вынести
+	dateLastActivity: '2022-11-10T19:11:31.293Z',
+	dateNextActivity: '2022-11-11T19:11:31.293Z',
+
+	nfcTagId: '', // 1, 2, 3, 4, ...
+	desc: 'Lorem ipsum is placeholder text commonly used in the graphic.',
+	name: 'Lorem impsum',
+	idLabels: [], // tags
+	bgColor: '',
+
+	// lean coffee
+	votes: 0,
+	idMembers: ['636d4c81150e4204agyua795f', '636d4c81150e4204agyua765f'],
+
+	// action delegation
+	idWhoDelegate: '636d4c6asd005570579feebb6',
+	idDelegated: '636d4c6730055721379feebb6',
+	img: '',
+
+	// health check
+	positiveDesc: 'Lorem ipsum is placeholder text commonly used in the graphic.',
+	negativeDesc: 'Lorem ipsum is placeholder text commonly used in the graphic.',
+	idMember: '636d4c6asdfg05570579feebb6',
+	dynamics: '', // up, down, unchanged - в зависимости от динамики карточки меняют цвет фона, ультра лёгкий 200-300
+
+	//   kanbanel
+	start: null,
+	due: null,
+	dueComplete: false,
+	summaryProfits: [
+		['1, 2, 3, 4', 480] // 1, 2, 3, 4 - time to market, 480 - profit
+	],
+	expectedProfit: '', // high, low, medium,
+	// labels: '', // feature, express ...
+	estimationFirstPhase: 8,
+	estimationSecondPhase: 4,
+	estimationThirdPhase: 6,
+	trigger: '636d4c6730055705782aybb6', // тригериться событие по ID
+	dor: 'Card №10 completed and delivered', // DoR
+	dod: 'Card №10 completed and delivered', // DoD
+
+	// pomodoro plan
+	// labels: '', // bussines education relations sport
+	expectedEsimate: 6,
+	realEstimate: 7
+};
+
+export default card;

7c0830ef5432def5a7899c25ff5d3bf087c0ee73
DarkDev <darkdev_brand@riseup.net>
2023-06-27 12:38:18 +0300

    alfa v.

--- src/routes/promo/+page.svelte
+++ src/routes/promo/+page.svelte
@@ -1,43 +1,140 @@
-<section>
-	<div class="mx-auto max-w-5xl px-4 py-12 sm:px-6 md:px-12 lg:px-24 lg:py-24">
-		<div class="mx-auto flex max-w-7xl flex-wrap items-center">
-			<div class="w-full rounded-xl lg:w-1/2 lg:max-w-lg">
-				<div>
-					<div class="relative w-full max-w-lg">
-						<div
-							class="animate-blob absolute top-0 -left-4 h-72 w-72 rounded-full bg-violet-300 opacity-70 mix-blend-multiply blur-xl filter"
-						/>
+<script>
+	const code = 'MEET06.20';
+</script>
 
-						<div
-							class="animate-blob animation-delay-4000 absolute -bottom-24 right-20 h-72 w-72 rounded-full bg-fuchsia-300 opacity-70 mix-blend-multiply blur-xl filter"
-						/>
-						<div class="relative">
-							<img
-								class="mx-auto rounded-lg object-cover object-center shadow-2xl"
-								alt="promocode"
-								src="/assets/images/placeholders/squareCard.png"
-							/>
-						</div>
-					</div>
-				</div>
-			</div>
+<section
+	class="min-h-screen bg-[url('https://ibvsolutions.com/wp-content/uploads/2019/02/Kanban-on-Screen-781x512.jpg')] bg-cover bg-[50%] bg-no-repeat py-20 "
+>
+	<!-- Jumbotron -->
+	<div
+		class="w-100 mx-auto px-6 sm:max-w-2xl md:max-w-3xl md:px-12 lg:max-w-4xl xl:max-w-7xl xl:px-32"
+	>
+		<div class="text-center">
 			<div
-				class="mt-12 mb-16 flex flex-col items-start text-left md:mb-0 lg:w-1/2 lg:flex-grow lg:pl-6 xl:mt-0 xl:pl-24"
+				class="block rounded-lg bg-[hsla(0,0%,100%,0.8)] px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-[hsla(0,0%,15%,0.8)] dark:shadow-black/20 md:py-16 md:px-12"
+				style="
+            backdrop-filter: saturate(200%) blur(6px);
+          "
 			>
-				<h1
-					class="mb-2 text-4xl font-bold leading-none tracking-tighter text-neutral-600 md:text-7xl lg:text-5xl"
+				<div
+					class="flex h-full flex-col items-center justify-center  px-2 py-4 text-center text-white lg:text-left"
 				>
-					Lorem ipsum is placeholder
-				</h1>
-				<p class="mb-2 text-left text-base leading-relaxed text-gray-500">
-					Lorem ipsum is placeholder text commonly used in the graphic, print,
-				</p>
-				<div class="mt-0  sm:flex lg:mt-4">
-					<div class="mt-3 rounded-lg sm:mt-0">
-						<button
-							class="block w-full transform items-center rounded-xl bg-pink-600 px-10 py-4 text-center text-base font-medium text-white transition duration-500 ease-in-out hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
-							>Activate promocode</button
+					<div class="flex flex-col items-center justify-center">
+						<!-- <a href="./index.html" class="text-medium text-pink-600">LOGO</a> -->
+						<h2 class="mt-6 mb-4 max-w-md text-center text-4xl font-extrabold text-white">
+							Aktywacja kodu promocyjnego
+						</h2>
+
+						<span
+							class="block rounded-xl bg-pink-600 py-4 px-6 text-center text-6xl font-black text-black"
+							>{code}</span
 						>
+					</div>
+					<div class="lg:pl-12">
+						<div class="w-full px-6 py-3">
+							<form action="#" method="POST" class="space-y-6">
+								<div class="grid grid-cols-1 gap-2 lg:grid-cols-2">
+									<div>
+										<label for="email" class="block text-sm font-medium text-neutral-100">
+											Email address
+										</label>
+										<div class="mt-1">
+											<input
+												id="email"
+												name="email"
+												type="email"
+												autocomplete="email"
+												required=""
+												placeholder="Your Email"
+												class="block w-full transform rounded-lg border border-transparent bg-gray-50 px-5 py-3 text-base text-black placeholder-gray-300 transition duration-500 ease-in-out focus:border-transparent focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
+											/>
+										</div>
+									</div>
+									<div class="space-y-1">
+										<label for="password" class="block text-sm font-medium text-neutral-100">
+											Password
+										</label>
+										<div class="mt-1">
+											<input
+												id="password"
+												name="password"
+												type="password"
+												autocomplete="current-password"
+												required=""
+												placeholder="Your Password"
+												class="block w-full transform rounded-lg border border-transparent bg-gray-50 px-5 py-3 text-base text-black placeholder-gray-300 transition duration-500 ease-in-out focus:border-transparent focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
+											/>
+										</div>
+									</div>
+								</div>
+
+								<div class="flex items-center justify-between">
+									<div class="flex items-center">
+										<input
+											id="remember-me"
+											name="remember-me"
+											type="checkbox"
+											placeholder="Your password"
+											class="h-4 w-4 rounded border-gray-200 text-black focus:ring-black"
+										/>
+										<label for="remember-me" class="ml-2 block text-sm text-neutral-100">
+											Remember me
+										</label>
+									</div>
+									<div class="text-sm">
+										<a href="#" class="font-medium text-pink-400 hover:text-black">
+											Forgot your password?
+										</a>
+									</div>
+								</div>
+								<div>
+									<button
+										type="submit"
+										class="flex w-full transform items-center justify-center rounded-xl bg-pink-600 px-10 py-4 text-center text-base font-medium text-white transition duration-500 ease-in-out hover:bg-black focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+										>Aktywować</button
+									>
+								</div>
+							</form>
+							<div class="relative my-4 ">
+								<div class="absolute inset-0 flex items-center">
+									<div class="w-full border-t border-gray-300 " />
+								</div>
+								<div class="relative flex justify-center rounded-xl text-sm">
+									<span class="bg-white px-2 text-black"> Or continue with </span>
+								</div>
+							</div>
+
+							<div>
+								<button
+									type="submit"
+									class="block w-full transform items-center rounded-xl border-2 border-white px-10 py-3.5 text-center text-base font-medium text-blue-600 shadow-md transition duration-500 ease-in-out focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+								>
+									<div class="flex items-center justify-center">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											xmlns:xlink="http://www.w3.org/1999/xlink"
+											class="h-6 w-6"
+											viewBox="0 0 48 48"
+										>
+											<defs>
+												<path
+													id="a"
+													d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"
+												/>
+											</defs>
+											<clipPath id="b">
+												<use xlink:href="#a" overflow="visible" />
+											</clipPath>
+											<path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" />
+											<path clip-path="url(#b)" fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z" />
+											<path clip-path="url(#b)" fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z" />
+											<path clip-path="url(#b)" fill="#4285F4" d="M48 48L17 24l-4-3 35-10z" />
+										</svg>
+										<span class="ml-4"> Log in with Google</span>
+									</div>
+								</button>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>

21e54c774985c53573c06522f46a75c1735fa2d1
DarkDev <darkdev_brand@riseup.net>
2023-07-10 14:29:50 +0300

    link for external service

--- src/lib/core/layout/shared/index.js
+++ src/lib/core/layout/shared/index.js
@@ -5,6 +5,15 @@ import FooterLink from './ui/FooterLink.svelte';
 import BtnLogIn from '../shared/ui/BtnLogIn.svelte';
 import BtnSignIn from '../shared/ui/BtnSignIn.svelte';
 import LinkBtnStyle from '../shared/ui/LinkBtnStyle.svelte';
+import FooterLinkTargetNewBlank from '../shared/ui/FooterLinkTargetNewBlank.svelte';
 
 
-export { ShowFullListBtn, BtnLogIn, BtnSignIn, PriceItem, List, FooterLink, LinkBtnStyle };
+export {
+	ShowFullListBtn,
+	FooterLinkTargetNewBlank, BtnLogIn,
+	BtnSignIn,
+	PriceItem,
+	List,
+	FooterLink,
+	LinkBtnStyle
+};
---
+++ src/lib/core/layout/shared/ui/FooterLinkTargetNewBlank.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let link;
+</script>
+
+<a
+	href={link.link} target="_blank"
+	class="text-neutral-400 transition-colors duration-200 hover:text-pink-600 hover:underline  "
+	>{link.title}</a
+>

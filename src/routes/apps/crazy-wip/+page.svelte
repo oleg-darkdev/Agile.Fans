1924243a19584bd4ccb1ff5c43841e6e2912a2fe
DarkDev <darkdev_brand@riseup.net>
2023-07-14 14:40:18 +0300

    intial

---
+++ src/routes/apps/crazy-wip/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { AppScreen } from '../../../lib/core/layout/widgets/';
+</script>
+
+<AppScreen>
+	<div slot="start-screen">
+		<h2>Text start</h2>
+	</div>
+	<div slot="manual-screen" />
+	<div slot="game-screen" />
+</AppScreen>

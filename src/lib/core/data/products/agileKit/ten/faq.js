1bf6b162dfad3f1a674f163044b8a404e3f33ee3
DarkDev <darkdev_brand@riseup.net>
2023-07-19 20:01:57 +0300

    upd content

--- src/lib/core/data/products/agileKit/ten/faq.js
+++ src/lib/core/data/products/agileKit/ten/faq.js
@@ -3,29 +3,39 @@ const faq = {
 
 	content: [
 		{
-			title: 'Lorem',
-			desc: ['Lorem', 'Lorem', 'Lorem', 'Lorem', 'Lorem']
+			title: 'Gdzie poza HR można korzystać z tego narzędzia?',
+			desc: [
+				'Narzędzie to jest również przydatne dla trenerów Agile i Scrum Masterów, istnieje kilka sposobów jego wykorzystania:',
+				'1. Aby rozpocząć głębszą dyskusję podczas retrospektywy.',
+				'2.  Aby zidentyfikować motywacje wszystkich członków zespołu i zbudować duży obraz.',
+				'3. Znalezienie indywidualnych motywatorów i porównanie ich z motywacją całego zespołu.',
+				'Chociaż dobrze pasuje do każdego zespołu, który chce zrozumieć motywację pracownika lub przyszłego pracownika, ćwiczenie to stało się szczególnie popularne w retrospektywach scrumowych, gdzie jest używane regularnie lub jako "mile widziane urozmaicenie" zwykłych retrospektyw.'
+			]
 		},
 		{
-			title: 'Lorem',
-			desc: ['Lorem', 'Lorem', 'Lorem', 'Lorem', 'Lorem']
+			title: 'Wskazówki dotyczące grania w #10',
+			desc: [
+				'1. Zawsze należy zaplanować dyskusję po zagraniu w #10. To właśnie w niej tkwi największa wartość, jeśli chodzi o poznanie motywacji pracowników.',
+				'2. Podczas gry w #10 niektórym osobom pomaga głośne myślenie podczas zwracania się do (milczącego) obserwatora.',
+				'3. Niektóre grupy uważają za przydatne rozpoczęcie od krótkiej dyskusji na temat 10 motywatorów, aby uzyskać węższe zrozumienie i interpretację każdego z nich.',
+			]
 		},
-		{
-			title: 'Lorem',
-			desc: ['Lorem', 'Lorem', 'Lorem', 'Lorem', 'Lorem']
-		},
-		{
-			title: 'Lorem',
-			desc: ['Lorem', 'Lorem', 'Lorem', 'Lorem', 'Lorem']
-		},
-		{
-			title: 'Lorem',
-			desc: ['Lorem', 'Lorem', 'Lorem', 'Lorem', 'Lorem']
-		},
-		{
-			title: 'Lorem',
-			desc: ['Lorem', 'Lorem', 'Lorem', 'Lorem', 'Lorem']
-		}
+		// {
+		// 	title: 'Lorem',
+		// 	desc: ['Lorem', 'Lorem', 'Lorem', 'Lorem', 'Lorem']
+		// },
+		// {
+		// 	title: 'Lorem',
+		// 	desc: ['Lorem', 'Lorem', 'Lorem', 'Lorem', 'Lorem']
+		// },
+		// {
+		// 	title: 'Lorem',
+		// 	desc: ['Lorem', 'Lorem', 'Lorem', 'Lorem', 'Lorem']
+		// },
+		// {
+		// 	title: 'Lorem',
+		// 	desc: ['Lorem', 'Lorem', 'Lorem', 'Lorem', 'Lorem']
+		// }
 	]
 };
 export default faq;

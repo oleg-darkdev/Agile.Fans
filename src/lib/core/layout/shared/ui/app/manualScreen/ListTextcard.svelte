3a0f7694b76cf0ad771ca31f2f9f8926dc366dfb
DarkDev <darkdev_brand@riseup.net>
2023-07-20 12:37:14 +0300

    add margin, font-bold

--- src/lib/core/layout/shared/ui/app/manualScreen/ListTextcard.svelte
+++ src/lib/core/layout/shared/ui/app/manualScreen/ListTextcard.svelte
@@ -6,9 +6,9 @@
 	class=" m-2 w-full max-w-2xl rounded-lg border-2 border-pink-700 bg-black p-4 text-white shadow-md shadow-pink-700 "
 >
 	<div class="flex py-6 px-2 flex-col pb-3">
-		<dt class="mb-1 text-pink-500  md:text-lg">{data.title}</dt>
+		<dt class="mb-4 text-pink-500 text-2xl font-bold  ">{data.title}</dt>
 		{#each data.desc as desc}
-			<dd class="text-lg font-semibold">{desc}</dd>
+			<dd class="text-lg  mb-2 font-semibold">{desc}</dd>
 		{/each}
 
 			<slot />

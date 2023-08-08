a692ce4855b5d6cbb94b1b91b8373d8f79df5db4
DarkDev <darkdev_brand@riseup.net>
2023-08-08 22:08:59 +0300

    refactoring

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,10 +1,7 @@
-<main>
-	<h1>Login</h1>
-	<form action="?/login" method="POST" class="auth-form">
-		<label for=""> Email </label>
-		<input type="text" name="email" />
-		<label for=""> Password </label>
-		<input type="password" name="password" />
-		<button type="submit" class="btn btn-primary">Login</button>
-	</form>
-</main>
+<script>
+	import { LoginForm } from '../../lib/core/layout/entities';
+
+	export let form;
+</script>
+
+<LoginForm bind:form />

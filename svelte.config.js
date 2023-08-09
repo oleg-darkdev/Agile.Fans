7a244b7b6cfeb4fa4fa3f136b435b593fd013cfa
DarkDev <darkdev_brand@riseup.net>
2023-08-09 19:13:05 +0300

    add aliases

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,28 @@
 import adapter from '@sveltejs/adapter-auto';
+import path from 'path';
 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-
-  kit: {
-    adapter: adapter()
-  }
+	kit: {
+		adapter: adapter()
+	},
+	vite: {
+		resolve: {
+			alias: {
+				// '@coreEntities': path.resolve('./src/lib/core/layout/entities'),
+				// '@coreFeatures': path.resolve('./src/lib/core/layout/features'),
+				// '@coreProcesses': path.resolve('./src/lib/core/layout/processes'),
+				// '@coreShared': path.resolve('./src/lib/core/layout/shared'),
+				// '@coreWidgets': path.resolve('./src/lib/core/layout/widgets'),
+				$retrogenAppWidgets: path.resolve('./src/lib/retrogen/app/widgets/'),
+				$retrogenAppEntities: path.resolve('./src/lib/retrogen/app/entities/'),
+				$retrogenAppFeatures: path.resolve('./src/lib/retrogen/app/features/'),
+				$retrogenAppProcesses: path.resolve('./src/lib/retrogen/app/processes/'),
+				$retrogenAppShared: path.resolve('./src/lib/retrogen/app/shared/')
+			}
+		}
+	}
 };
 
 export default config;

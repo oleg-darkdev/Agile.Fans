02572e1873b009a923b2a26c6ae7db0253f6df2a
DarkDev <darkdev_brand@riseup.net>
2023-08-09 19:13:54 +0300

    alfa v.

---
+++ src/lib/retrogen/app/entities/ui/ComponentsTable.svelte
@@ -0,0 +1,50 @@
+<script>
+	export let components;
+
+	console.log(components);
+</script>
+
+<div class="w-full overflow-x-auto">
+	<table class="table w-full">
+		<!-- head -->
+		<thead>
+			<tr>
+				<!-- <th /> -->
+				<th>ID</th>
+
+				<th>Title</th>
+				<th>Count</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each components as component}
+				<tr class="text-black">
+					<td>
+						{component.id}
+					</td>
+					<td>
+						<div class="flex items-center space-x-3">
+							<div class="avatar">
+								<div class="mask mask-squircle h-12 w-12">
+									<img src={component.photo} alt="Avatar Tailwind CSS Component" />
+								</div>
+							</div>
+							<div>
+								<div class="font-bold">{component.title}</div>
+								<div class="text-sm ">{component.shortDesc}</div>
+							</div>
+						</div>
+					</td>
+					<td>{component.count}</td>
+				</tr>
+			{/each}
+		</tbody>
+		<tfoot>
+			<tr>
+				<th>ID</th>
+				<th>Title</th>
+				<th>Count</th>
+			</tr>
+		</tfoot>
+	</table>
+</div>
---
+++ src/lib/retrogen/app/entities/ui/TextInstruction.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let title, desc, steps;
+</script>
+
+<div class=" mx-auto flex max-w-7xl flex-col items-center px-5  sm:px-6 lg:px-8">
+	<div class="prose-blue prose mx-auto flex w-full max-w-3xl flex-col text-left">
+		<div class="mx-auto w-full">
+			<h2>{title}</h2>
+			<p>
+				{desc}
+			</p>
+		</div>
+	</div>
+	{#each steps as instructionStep, step}
+		<div class="mx-auto my-2 flex w-full flex-col items-center  sm:flex-row">
+			<div
+				class="mr-4 inline-flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full bg-pink-700 group-hover:text-pink-500 group-hover:bg-neutral-50  text-neutral-50"
+			>
+				<span> {step + 1} </span>
+			</div>
+			<div class="prose-md text-pink-500 group-hover:text-neutral-50 prose mt-6 flex-grow text-center sm:mt-0 sm:text-left">
+				<p>
+					{instructionStep}
+				</p>
+			</div>
+		</div>
+	{/each}
+</div>

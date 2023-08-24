85af2114ef4afcfceca76e3f13afe0c2d31b0e72
DarkDev <darkdev_brand@riseup.net>
2023-08-24 14:16:12 +0300

    refactoring table in to the list

--- src/lib/retrogen/app/entities/ui/ComponentsTable.svelte
+++ src/lib/retrogen/app/entities/ui/ComponentsTable.svelte
@@ -1,50 +1,40 @@
 <script>
 	export let components;
-
-	console.log(components);
 </script>
 
-<div class="w-full overflow-x-auto">
-	<table class="table w-full">
-		<!-- head -->
-		<thead>
-			<tr>
-				<!-- <th /> -->
-				<th>ID</th>
 
-				<th>Title</th>
-				<th>Count</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each components as component}
-				<tr class="text-black">
-					<td>
-						{component.id}
-					</td>
-					<td>
-						<div class="flex items-center space-x-3">
-							<div class="avatar">
-								<div class="mask mask-squircle h-12 w-12">
-									<img src={component.photo} alt="Avatar Tailwind CSS Component" />
-								</div>
-							</div>
-							<div>
-								<div class="font-bold">{component.title}</div>
-								<div class="text-sm ">{component.shortDesc}</div>
-							</div>
-						</div>
-					</td>
-					<td>{component.count}</td>
-				</tr>
-			{/each}
-		</tbody>
-		<tfoot>
-			<tr>
-				<th>ID</th>
-				<th>Title</th>
-				<th>Count</th>
-			</tr>
-		</tfoot>
-	</table>
+<div
+	class="flex max-w-sm flex-col divide-gray-200 rounded-lg  bg-white p-4 text-gray-900 shadow-md  lg:max-w-lg"
+>
+	<div class="mb-4 flex items-center justify-between">
+		<h5 class="text-xl font-bold leading-none ">Components</h5>
+
+	</div>
+	<ul class="divide-y divide-gray-200 rounded-lg border-0 border-gray-200 bg-white text-gray-900 ">
+		{#each components as component}
+			<li
+				class="w-full list-none py-2 px-4 text-sm font-medium first:rounded-t-lg last:rounded-b-lg"
+			>
+				<div class="flex items-center space-x-4">
+					{component.id}
+
+					<img
+						alt="Neil Sims"
+						src={component.photo}
+						class="h-10 w-10 flex-shrink-0 rounded-full bg-gray-100 text-gray-600 "
+					/>
+
+					<div class="min-w-0 flex-1">
+						<p class="truncate text-sm font-medium ">
+							{component.title}
+						</p>
+						<!-- <p class="truncate text-sm text-gray-900 ">{component.shortDesc}</p> -->
+					</div>
+					<div class="inline-flex items-center text-base font-semibold ">
+						{component.count}
+					</div>
+				</div>
+			</li>
+		{/each}
+	</ul>
 </div>

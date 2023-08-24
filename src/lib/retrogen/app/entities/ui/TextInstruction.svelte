a25ea86ade22cdf050d872f3bc20666ce3a50299
DarkDev <darkdev_brand@riseup.net>
2023-08-24 14:16:42 +0300

    adaptive for mobile

--- src/lib/retrogen/app/entities/ui/TextInstruction.svelte
+++ src/lib/retrogen/app/entities/ui/TextInstruction.svelte
@@ -1,8 +1,10 @@
 <script>
-  export let title, desc, steps;
+	export let title, desc, steps;
 </script>
 
-<div class=" mx-auto flex max-w-7xl flex-col items-center px-5  sm:px-6 lg:px-8">
+<div
+	class=" mx-auto group my-2 flex max-w-7xl flex-col items-center rounded-xl border-2 border-pink-700 px-6 hover:bg-pink-700  hover:text-white lg:px-8"
+>
 	<div class="prose-blue prose mx-auto flex w-full max-w-3xl flex-col text-left">
 		<div class="mx-auto w-full">
 			<h2>{title}</h2>
@@ -12,13 +14,17 @@
 		</div>
 	</div>
 	{#each steps as instructionStep, step}
-		<div class="mx-auto my-2 flex w-full flex-col items-center  sm:flex-row">
+		<div
+			class="mx-auto my-2 flex w-full flex-col  justify-start md:flex-row  lg:flex-row "
+		>
 			<div
-				class="mr-4 inline-flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full bg-pink-700 group-hover:text-pink-500 group-hover:bg-neutral-50  text-neutral-50"
+				class="mr-4 inline-flex h-10 w-10  flex-shrink-0 items-center justify-center rounded-full bg-pink-700 text-neutral-50 group-hover:bg-neutral-50  group-hover:text-pink-500"
 			>
 				<span> {step + 1} </span>
 			</div>
-			<div class="prose-md text-pink-500 group-hover:text-neutral-50 prose mt-6 flex-grow text-center sm:mt-0 sm:text-left">
+			<div
+				class="prose-md xprose mb-2 flex-grow text-start  text-pink-500 group-hover:text-neutral-50 sm:mt-0 sm:text-left md:mt-6 lg:mt-6"
+			>
 				<p>
 					{instructionStep}
 				</p>
--- src/routes/apps/retrogen/+page.svelte
+++ src/routes/apps/retrogen/+page.svelte
@@ -1,184 +1,114 @@
 <script>
 	import { allScanariousForRetrogen } from '../../../lib/retrogen/app/shared';
+	import { LargeBtn } from '../../../lib/core/layout/shared/';
 	// '$coreShared'
 	import { ComponentsTable, TextInstruction } from '../../../lib/retrogen/app/entities';
 
 	$: selectedScenario = '';
+	$: showFullInstruction = true;
 </script>
 
-<!-- <section> -->
-<!-- <div class=" mx-auto flex max-w-7xl flex-col items-center px-5 py-8 sm:px-6 lg:px-8">
-	<div class="prose-blue prose mx-auto flex w-full max-w-3xl flex-col text-left">
-		<div class="mx-auto w-full">
-			<h1>A small headline to switch your visitors into users.</h1>
-			<h2>A small headline to switch your visitors into users.</h2>
-			<p>
-				Right. Say that again. No, no, George, look, it's just an act, right? Okay, so 9:00 you're
-				strolling through the parking lot, you see us struggling.
-			</p>
-		</div>
-	</div>
-
+<section class=" flex w-full justify-center py-20">
 	<div
-		class="mx-auto my-10 flex max-w-7xl flex-col items-center border-b border-gray-200 pb-10 sm:flex-row"
+		class="mx-4  mx-4 h-auto max-w-sm rounded rounded-xl bg-primary py-5 md:max-w-2xl md:px-6 lg:max-w-4xl lg:px-8"
 	>
-		<div
-			class="inline-flex h-20 w-20 flex-shrink-0 items-center justify-center rounded-full bg-gray-50 text-neutral-600 sm:mr-10"
-		>
-			<svg
-				fill="none"
-				stroke="currentColor"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="1.5"
-				class="h-10 w-10"
-				viewBox="0 0 24 24"
-			>
-				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-				<circle cx="12" cy="12" r="9" />
-				<line x1="3.6" y1="15" x2="14.15" y2="15" />
-				<line x1="3.6" y1="15" x2="14.15" y2="15" transform="rotate(72 12 12)" />
-				<line x1="3.6" y1="15" x2="14.15" y2="15" transform="rotate(144 12 12)" />
-				<line x1="3.6" y1="15" x2="14.15" y2="15" transform="rotate(216 12 12)" />
-				<line x1="3.6" y1="15" x2="14.15" y2="15" transform="rotate(288 12 12)" />
-			</svg>
-		</div>
-		<div class="prose-md prose mt-6 flex-grow text-center sm:mt-0 sm:text-left">
-			<h2>Short length headline.</h2>
-			<p>
-				Free and Premium themes, UI Kit's, templates and landing pages built with Tailwind CSS, HTML
-				&amp; Next.js.
-			</p>
-		</div>
-	</div>
-	<div
-		class="mx-auto my-10 flex max-w-7xl flex-col items-center border-b border-gray-200 pb-10 sm:flex-row"
-	>
-		<div
-			class="inline-flex h-20 w-20 flex-shrink-0 items-center justify-center rounded-full bg-gray-50 text-neutral-600 sm:mr-10"
-		>
-			<svg
-				fill="none"
-				stroke="currentColor"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="1.5"
-				class="h-10 w-10"
-				viewBox="0 0 24 24"
-			>
-				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-				<circle cx="12" cy="12" r="9" />
-				<line x1="3.6" y1="15" x2="14.15" y2="15" />
-				<line x1="3.6" y1="15" x2="14.15" y2="15" transform="rotate(72 12 12)" />
-				<line x1="3.6" y1="15" x2="14.15" y2="15" transform="rotate(144 12 12)" />
-				<line x1="3.6" y1="15" x2="14.15" y2="15" transform="rotate(216 12 12)" />
-				<line x1="3.6" y1="15" x2="14.15" y2="15" transform="rotate(288 12 12)" />
-			</svg>
-		</div>
-		<div class="prose-md prose mt-6 flex-grow text-center sm:mt-0 sm:text-left">
-			<h2>Short length headline.</h2>
-			<p>
-				Free and Premium themes, UI Kit's, templates and landing pages built with Tailwind CSS, HTML
-				&amp; Next.js.
-			</p>
-		</div>
-	</div>
-</div> -->
+		<!-- <div class="grid gap-6 lg:grid-cols-2"> -->
 
-<section
-	class="mx-4 h-auto max-w-4xl justify-center rounded rounded-xl bg-primary py-5 md:px-6 lg:px-8"
->
-	<!-- <div class="grid gap-6 lg:grid-cols-2"> -->
-	<ul class="flex flex-row flex-wrap justify-center ">
-		{#each allScanariousForRetrogen as scenario, i}
-			<li
-				on:click={() => {
-					scenario.showFull = !scenario.showFull;
-					selectedScenario = scenario;
-				}}
-				class="group m-2  w-full rounded-xl border-2 border-pink-700 p-3 transition duration-300 ease-in-out hover:bg-pink-700 hover:text-white"
-			>
-				<!-- <a href="/apps/retrogen/allScanariousForRetrogen/{scenario.link}"> -->
-				<div class="flex items-start">
-					<div class="shrink-0">
-						<div class="flex  flex-row  pr-3">
-							<span class="mr-2 text-4xl  font-black text-white"> ID </span>
-							<div class="flex h-10 w-10 items-center justify-center rounded-xl bg-pink-700">
-								<span class="text-2xl font-black  text-white">
-									{scenario.id}
-								</span>
+		<ul id="start" class="flex flex-row flex-wrap justify-center px-8 ">
+			{#if !selectedScenario && showFullInstruction}
+				{#each allScanariousForRetrogen as scenario, i}
+					<li
+						on:click={() => {
+							scenario.showFull = !scenario.showFull;
+							selectedScenario = scenario;
+							showFullInstruction = !showFullInstruction;
+							location.href = '#start';
+						}}
+						class="m-2 flex w-full flex-row flex-row-reverse  flex-wrap justify-between {scenario.showFull
+							? ''
+							: ' border-2 border-pink-700 hover:bg-pink-700 hover:text-white'} rounded-xl p-3 transition duration-300 ease-in-out  "
+					>
+						<!-- <a href="/apps/retrogen/allScanariousForRetrogen/{scenario.link}"> -->
+						<a class="" href={scenario.author.link}>Author: <span>{scenario.author.bio}</span></a>
+
+						<div class="mb-4 flex flex-col items-start md:mb-0 lg:mb-0">
+							<div class="shrink-0">
+								<div class="flex  flex-row  pr-3">
+									<span class="mr-2 text-4xl  font-black text-white"> ID </span>
+									<div class="flex h-10 w-10 items-center justify-center rounded-xl bg-pink-700">
+										<span class="text-2xl font-black  text-white">
+											{scenario.id}
+										</span>
+									</div>
+								</div>
+							</div>
+							<div class="group grow ">
+								<p class="mb-1 font-bold text-pink-700 group-hover:text-white">
+									{scenario.title}
+								</p>
+								<p class="mb-0 text-pink-500 group-hover:text-neutral-50">
+									{scenario.shortDesc}
+								</p>
 							</div>
 						</div>
-					</div>
-					<div class="group grow">
-						<p class="mb-1 font-bold text-pink-700 group-hover:text-white">
-							{scenario.title}
-						</p>
-						<p class="mb-0 text-pink-500 group-hover:text-neutral-50">
-							{scenario.shortDesc}
-						</p>
-					</div>
-				</div>
-				<a href={scenario.author.link}>Author: <span>{scenario.author.bio}</span></a>
-
-				<!-- </a> -->
-				{#if scenario.showFull}
-					{#each scenario.desc as desc}
-						<p class="mb-0 text-pink-500 group-hover:text-neutral-50">
+						<!-- {
+			title: 'Complect for tool: `Start, Stop, Continue`',
+			id: '',
+			photo: ''
+		}, -->
+					</li>
+				{/each}
+			{:else}
+				<button
+					on:click={() => {
+						showFullInstruction = !showFullInstruction;
+						selectedScenario = '';
+					}}
+					class="btn-lg btn mx-6 mx-auto mt-6 mt-4 w-8/12 max-w-2xl bg-pink-600"
+					>Show all scenarios</button
+				>
+				<div
+					class="m-2 rounded-xl border-2 border-pink-700 p-3 text-pink-500 text-pink-500 hover:bg-pink-700 hover:text-white"
+				>
+					{#each selectedScenario.desc as desc}
+						<p class="mb-0  ">
 							{desc}
 						</p>
 					{/each}
+				</div>
 
-					<div class=" mx-auto flex max-w-7xl flex-col items-center px-5  sm:px-6 lg:px-8">
-						<div class=" mx-auto flex w-full max-w-3xl flex-col text-left">
-							<div class="mx-auto w-full">
-								<h2 class="mb-4 text-2xl font-bold">Components</h2>
-								<ComponentsTable components={scenario.components} />
-							</div>
+				<div class=" mx-auto flex flex-col items-center px-5 sm:px-6  lg:max-w-7xl lg:px-8">
+					<div class=" lg: mx-auto flex w-full  max-w-3xl flex-col text-left">
+						<div class="mx-auto w-full">
+							<ComponentsTable components={selectedScenario.components} />
 						</div>
 					</div>
+				</div>
 
-					<TextInstruction
-						title="Preparation"
-						desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing
+				<TextInstruction
+					title="Preparation"
+					desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing
 				industries for previewing layouts and visual mockups."
-						steps={scenario.preparation}
-					/>
+					steps={selectedScenario.preparation}
+				/>
 
-					<TextInstruction
-						title="Instruction"
-						desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing
+				<TextInstruction
+					title="Instruction"
+					desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing
 				industries for previewing layouts and visual mockups."
-						steps={scenario.instruction}
-					/>
+					steps={selectedScenario.instruction}
+				/>
 
-					<!-- export let
-</script>
-
-<!--    -->
-					<!-- preparation: [
-		`1. Создайте из 3 секций 3 колонки`,
-		`2. Найдите набор компонентов с идентификатором # в пакете с логотипом retrogen.`,
-		`3. Озаглавьте колонки: 'Начать', 'Прекратить' и 'Продолжать`,
-		`4. Ознакомьтесь с инструкцией`,
-		`5. Запускайте приложение`,
-		`6. `
-	],
-	instruction: [
-		`1. Установите таймер на 5 минут и попросите соблюдать тишину.`,
-		`2. Попросите участников написать свои предложения в каждую из колонок - отдельное предложение на отдельном стикере.`,
-		`3. Попросите участников зачитать свои предложения и разместить их на соотвествующих секциях.`,
-		`4. Организуйте короткую дискуссию: какие 20% предложений наиболее ценные. `,
-		`5. Затем попросите участников проголосовать точками (по 3 точки на человека).`,
-		`6. 3 предложения, набравшие наибольшее количество голосов, превращаются в план действий.`
-	], -->
-					<!-- photos: ['', ''],
-	video: '', -->
-					<!-- pomodoro timer -->
-				{/if}
-			</li>
-		{/each}
-	</ul>
-
-	<!-- </div> -->
+				<!-- pomodoro timer -->
+				<button
+					on:click={() => {
+						showFullInstruction = !showFullInstruction;
+						selectedScenario = '';
+					}}
+					class="btn-lg btn mx-6 mx-auto mt-6 mt-4 w-8/12 max-w-2xl bg-pink-600"
+					>Show all scenarios</button
+				>
+			{/if}
+		</ul>
+	</div>
 </section>

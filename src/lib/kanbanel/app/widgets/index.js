fd58285ce912c4883eb2c88d7c318b8a5bc405bd
DarkDev <darkdev_brand@riseup.net>
2023-08-24 22:56:08 +0300

    refactoring components for app kanbanel

--- src/lib/kanbanel/app/widgets/index.js
+++ src/lib/kanbanel/app/widgets/index.js
@@ -1,3 +1,9 @@
 import Board from './ui/Board.svelte';
+// import GameScreen from './ui/GameScreen.svelte';
+import StartScreen from './ui/StartScreen.svelte';
+import GameProgressScreen from './ui/GameProgressScreen.svelte';
+import ManualScreen from './ui/ManualScreen.svelte';
 
-export { Board };
+
+export { StartScreen, GameProgressScreen, ManualScreen, Board };
+

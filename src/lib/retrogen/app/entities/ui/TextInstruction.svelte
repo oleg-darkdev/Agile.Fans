d6ccaea7ae67ee706a255fa44e13a7fe7142c1a5
DarkDev <darkdev_brand@riseup.net>
2023-08-25 03:53:22 +0300

    add hide/show btn

--- src/lib/retrogen/app/entities/ui/TextInstruction.svelte
+++ src/lib/retrogen/app/entities/ui/TextInstruction.svelte
@@ -1,4 +1,6 @@
 <script>
+
+  let showFullInstruction =  false;
 	export let title, desc, steps;
 </script>
 
@@ -13,6 +15,13 @@
 			</p>
 		</div>
 	</div>
+
+  <button
+					on:click={() => showFullInstruction = !showFullInstruction}
+					class="group btn-lg btn mx-6 mx-auto mt-6 mb-4 w-8/12 max-w-2xl bg-pink-600 group-hover:bg-black"
+					>{showFullInstruction ? 'Hide': 'Show'}</button
+				> 
+        {#if showFullInstruction }
 	{#each steps as instructionStep, step}
 		<div
 			class="mx-auto my-2 flex w-full flex-col  justify-start md:flex-row  lg:flex-row "
@@ -31,4 +40,5 @@
 			</div>
 		</div>
 	{/each}
+  {/if}
 </div>

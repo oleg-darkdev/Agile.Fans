d48d806be9dd6766e53a0deb5302b5393ea3f89c
DarkDev <darkdev_brand@riseup.net>
2023-08-25 16:28:33 +0300

    refactoring

--- src/routes/apps/retrogen/tools/[slug]/+page.svelte
+++ src/routes/apps/retrogen/tools/[slug]/+page.svelte
@@ -1,52 +1,139 @@
 <script>
+	import Icon from 'svelte-awesome';
+	import timesRectangleO from 'svelte-awesome/icons/timesRectangleO';
+	import shareSquareO from 'svelte-awesome/icons/shareSquareO';
+	import plusCircle from 'svelte-awesome/icons/plusCircle';
+	import { selectedScenarious } from '@appRetrogenShared';
+	import playCircleO from 'svelte-awesome/icons/playCircleO';
+
+	let scenarioAdded = false;
+	const addScenarioToApp = (retrogenScenarion) => {
+		showGreenMsg = !showGreenMsg;
+		textMsg = 'The scenario has been added';
+		scenarioAdded = !scenarioAdded;
+		selectedScenarious.set($selectedScenarious.concat(retrogenScenarion));
+		setTimeout(hideGoodMsgWithTimer, 2000);
+	};
+
+	import { browser } from '$app/environment';
+
+	let showGreenMsg = false,
+		showRedMsg = false;
+	let showSharedBlock = true;
+
+	let textMsg = '',
+		currentURL = '';
+
+	if (browser) {
+		currentURL = window.location.href;
+	}
+
+	const hideGoodMsgWithTimer = () => (showGreenMsg = !showGreenMsg);
+	const hideBadMsgWithTimer = () => (showGreenMsg = !showGreenMsg);
+
+	const getSharedLink = () =>
+		navigator.clipboard
+			.writeText(currentURL)
+			.then(function () {
+				showGreenMsg = !showGreenMsg;
+				textMsg = 'Link copied';
+
+				setTimeout(hideGoodMsgWithTimer, 2000);
+			})
+			.catch(function (error) {
+				textMsg = error;
+				showRedMsg = !showRedMsg;
+
+				setTimeout(hideBadMsgWithTimer, 2000);
+			});
+
 	import { LargeBtn } from '@coreSharedLayout';
-	// '$coreShared'
 	import { ComponentsTable, TextInstruction } from '@appRetrogenEntities';
+	import { MsgToasts } from '@coreEntities';
 
-     export let data;
+	export let data;
 </script>
-
 
 <section class=" flex w-full justify-center py-20">
 	<div
-		class="mx-4  mx-4 h-auto max-w-sm rounded rounded-xl bg-primary py-5 md:max-w-2xl md:px-6 lg:max-w-4xl lg:px-8"
+		class="mx-4  flex h-auto max-w-sm flex-col items-center rounded rounded-xl bg-primary py-5 md:max-w-2xl md:px-6 lg:max-w-4xl lg:px-8"
 	>
-<div
-					class="m-2 rounded-xl flex flex-row  border-2 border-pink-700 p-3 text-pink-500 text-pink-500 hover:bg-pink-700 hover:text-white"
+		<div
+			class="m-2 flex flex-row rounded-xl  border-2 border-pink-700 p-3 text-pink-500 text-pink-500 hover:bg-pink-700 hover:text-white"
+		>
+			<div>
+				{#each data.tool.desc as desc}
+					<p class="mb-0  ">
+						{desc}
+					</p>
+				{/each}
+			</div>
+			<div class="">
+				{#if !scenarioAdded}
+					<button
+						on:click={() => {
+							addScenarioToApp(data.tool);
+							console.log($selectedScenarious.length);
+						}}
+						title="Add a scenario to the application"
+						class="{showSharedBlock ? 'mr-2' : 'mb-1'} "
+					>
+						<Icon data={plusCircle} scale={3} />
+					</button>
+				{:else}
+					<a
+						href="../"
+						class="{showSharedBlock ? 'mr-2' : 'mb-1'} "
+						title="Go to to the application"
+					>
+						<Icon data={playCircleO} scale={3} />
+					</a>
+				{/if}
+
+				<button
+					title="Share the retrospective scenario"
+					class={showSharedBlock ? 'mr-2' : 'mb-1'}
+					on:click={() => (showSharedBlock = !showSharedBlock)}
 				>
-					<div> {#each data.tool.desc as desc}
-						<p class="mb-0  ">
-							{desc}
-						</p>
-					{/each}</div>
-          <a href='../' class="btn btn-square btn-white btn-outline ">
-  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
-          </a>
-				</div>
+					<Icon data={shareSquareO} scale={3} />
+				</button>
+				<a href="../" title="Back to the list of scenarios">
+					<Icon data={timesRectangleO} scale={3} />
+				</a>
 
+				{#if showSharedBlock}
+					<input
+						type="text"
+						placeholder={currentURL}
+						on:click={() => {
+							getSharedLink();
 
+							showSharedBlock = !showSharedBlock;
+						}}
+						class="input-ghost input mt-6 w-full max-w-xs"
+					/>
+				{/if}
+			</div>
+		</div>
 
+		<ComponentsTable components={data.tool.components} />
 
-				<ComponentsTable components={data.tool.components} />
-
-				<TextInstruction
-					title="Preparation"
-					desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing
+		<TextInstruction
+			title="Preparation"
+			desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing
 				industries for previewing layouts and visual mockups."
-					steps={data.tool.preparation}
-				/>
+			steps={data.tool.preparation}
+		/>
 
-				<TextInstruction
-					title="Instruction"
-					desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing
+		<TextInstruction
+			title="Instruction"
+			desc="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing
 				industries for previewing layouts and visual mockups."
-					steps={data.tool.instruction}
-				/>
+			steps={data.tool.instruction}
+		/>
 
-				<a href=''
-					
-					class="btn-lg btn mx-6 mx-auto mt-6 mb-4 w-8/12 max-w-2xl bg-pink-600"
-					>Go to app</a
-				> 
-        </div>
-      </section>
+		<a href="" class="btn-lg btn mx-6 mx-auto mt-6 mb-4 w-8/12 max-w-2xl bg-pink-600">Go to app</a>
+	</div>
+
+	<MsgToasts bind:showGreenMsg bind:showRedMsg bind:textMsg />
+</section>

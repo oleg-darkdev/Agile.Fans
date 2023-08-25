650d4b8797f76a961669f442fc7e36348c87ce9f
DarkDev <darkdev_brand@riseup.net>
2023-08-25 16:28:05 +0300

    alfa v.

---
+++ src/lib/retrogen/app/widgets/ui/GameProgressScreen.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { selectedScenarious } from '@appRetrogenShared';
+	let arrSelectedScenarious = [];
+
+	selectedScenarious.subscribe((value) => {
+		arrSelectedScenarious = value;
+	});
+</script>
+
+<h2 class="text-white">
+	Выбрано сценариев для ретроспективы:
+
+	{arrSelectedScenarious.length}
+</h2>
+
+<!-- {#each arrSelectedScenarious as scencario}
+	<span>scen</span>
+{/each} -->

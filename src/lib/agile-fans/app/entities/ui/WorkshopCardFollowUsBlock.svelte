76a71a54fe4025f1f17826aad691ef5e1fab4f95
DarkDev <darkdev_brand@riseup.net>
2023-08-29 16:43:33 +0300

    fix for sm

--- src/lib/agile-fans/app/entities/ui/WorkshopCardFollowUsBlock.svelte
+++ src/lib/agile-fans/app/entities/ui/WorkshopCardFollowUsBlock.svelte
@@ -9,14 +9,15 @@
 	export let links;
 </script>
 
+<!-- rounded-lg border-2 border-pink-500 -->
 <div
-	class="mb-6 flex  flex-col rounded-lg border-2 border-pink-500 px-2 py-2 group-hover:border-black"
+	class="mb-6 flex  flex-col  px-2 py-2 group-hover:border-black"
 >
 	<h3 class="text-lg group-hover:text-black">Follow us</h3>
-	<div class="flex w-10/12 flex-row flex-wrap justify-between">
-		<Icon data={meetup} scale={3} class="group-hover:fill-black" />
-		<Icon data={linkedinSquare} scale={3} class="group-hover:fill-black" />
+	<div class="flex w-[120px] flex-row flex-wrap justify-between">
+		<Icon data={meetup} scale={2} class="group-hover:fill-black" />
+		<Icon data={linkedinSquare} scale={2} class="group-hover:fill-black" />
 		<!-- <Icon data={questionCircleO} scale={3} class='group-hover:fill-black'/> -->
-		<Icon data={shareSquareO} scale={3} class="group-hover:fill-black" />
+		<Icon data={shareSquareO} scale={2} class="group-hover:fill-black" />
 	</div>
 </div>

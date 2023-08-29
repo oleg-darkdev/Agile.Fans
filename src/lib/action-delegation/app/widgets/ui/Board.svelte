736b73b04aa3ba3fee11688b928fa1d0a80f3a71
DarkDev <darkdev_brand@riseup.net>
2023-08-29 16:41:42 +0300

    clear, refactoring

--- src/lib/action-delegation/app/widgets/ui/Board.svelte
+++ src/lib/action-delegation/app/widgets/ui/Board.svelte
@@ -1,98 +1,48 @@
 <script>
-	import { BoardCard, BoardColumn } from '../../entities';
+	import { BoardColumn } from '../../entities';
 
-	const cardLorem = {
-		type: 'Design',
-		date: 'Dec 12',
-		members: [
-			{
-				fullName: '',
-				email: '',
-				avatar: 'https://randomuser.me/api/portraits/women/26.jpg'
-			},
-			{
-				fullName: '',
-				email: '',
-				avatar: 'https://randomuser.me/api/portraits/women/25.jpg'
-			}
-		],
-		title: 'Lorem ipsum dolor',
-		desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-	};
-	const board = {
-		title: '',
-		columns: [
-			{
-				title: 'Backlog',
-				data: [
-					cardLorem,
-					cardLorem,
-					cardLorem,
-					cardLorem,
-					cardLorem,
-					cardLorem,
-					cardLorem,
-					cardLorem
-				],
-				id: 0
-			},
-			{
-				title: 'Level 1',
-				data: [cardLorem, cardLorem],
-				id: 1
-			},
-			{
-				title: 'Level 2',
-				data: [cardLorem, cardLorem, cardLorem],
-				id: 2
-			},
-			{
-				title: 'Level 3',
-				data: [cardLorem],
-				id: 3
-			},
-			{
-				title: 'Level 4',
-				data: [cardLorem, cardLorem, cardLorem, cardLorem, cardLorem],
-				id: 4
-			},
-			{
-				title: 'Level 5',
-				data: [cardLorem, cardLorem, cardLorem],
-				id: 5
-			},
-			{
-				title: 'Level 6',
-				data: [cardLorem, cardLorem],
-				id: 6
-			},
-			{
-				title: 'Level 7',
-				data: [cardLorem, cardLorem, cardLorem, cardLorem, cardLorem, cardLorem],
-				id: 7
-			}
-		]
-	};
+	// const cardLorem = {
+	// 	type: 'Design',
+	// 	tags: ['', '', '', '', '', ''],
+	//   value: '',
+	// 	date: 'Dec 12',
+	// 	members: [
+	// 		{
+	// 			fullName: '',
+	// 			email: '',
+	// 			avatar: 'https://randomuser.me/api/portraits/women/26.jpg'
+	// 		},
+	// 		{
+	// 			fullName: '',
+	// 			email: '',
+	// 			avatar: 'https://randomuser.me/api/portraits/women/25.jpg'
+	// 		}
+	// 	],
+	// 	title: 'Lorem ipsum dolor',
+	// 	desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+	// };
 
-	let showCards = false;
+
+
+
+	export let start, selectedBoard;
 </script>
 
-<div
-	class="flex min-h-screen w-screen justify-center  bg-gradient-to-t from-pink-200 via-pink-400 to-pink-500 text-gray-700"
->
-	<!-- <div class="px-10">
-		<h1 class="text-2xl font-bold text-neutral-200">Team Project Board</h1>
-	</div> -->
-	<div
-		class="mt-4 flex {showCards
-			? 'flex-col'
-			: 'h-80 max-w-7xl flex-row flex-wrap items-center justify-center'} overflow-hidden  px-4 "
-	>
-		{#each board.columns as column (column.id)}
-			<BoardColumn {column} bind:showCards />
-		{/each}
+<!-- bg-gradient-to-t from-black via-pink-400 to-pink-500 -->
 
-		<div class="w-6 flex-shrink-0" />
+<div
+	style="height: 100%;"
+	on:click={() => (start = true)}
+	class="flex w-full flex-col justify-center text-gray-700"
+>
+	<div class="px-10">
+		<h1 class="text-2xl font-bold text-neutral-200">{start ? selectedBoard.title: 'Task allocation'}</h1>
+	</div>
+	<div class="flex-row flex-wrap justify-center">
+		<div class=" flex  flex-col items-center  justify-center  px-4 ">
+			{#each selectedBoard.columns as column (column.id)}
+				<BoardColumn {column} />
+			{/each}
+		</div>
 	</div>
 </div>
-<!-- Component End -->

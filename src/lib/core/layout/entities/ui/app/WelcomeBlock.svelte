880fea42c15bcd2ea6fcb56c0b52afad94f7045d
DarkDev <darkdev_brand@riseup.net>
2023-09-01 21:40:39 +0300

    alfa v.

---
+++ src/lib/core/layout/entities/ui/app/WelcomeBlock.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { AppBlocksWrapper } from '@coreSharedLayout';
+
+	export let title, desc, video, step;
+</script>
+
+<AppBlocksWrapper>
+	<div class="py-6">
+		<div class="mx-auto max-w-screen-xl px-4 md:px-8 md:text-center ">
+			<div class="mb-4 max-w-xl md:mx-auto">
+				<h3 class="text-3xl font-bold text-gray-100 sm:text-4xl">{title}</h3>
+				<p class="mt-3 text-gray-100">
+					{desc}
+				</p>
+			</div>
+
+			<video
+				class="w-full rounded-2xl"
+				poster={video.poster}
+				controls="true"
+				playsinline
+				muted
+				loop
+			>
+				<source src={video.link} type="video/mp4" />
+			</video>
+
+			<div class="mt-4 flex items-center justify-center">
+				<button class="btn-lg  btn mx-6 mt-4 max-w-2xl bg-pink-600" on:click={() => step++}
+					>Dalej
+				</button>
+			</div>
+		</div>
+	</div>
+</AppBlocksWrapper>

0e26e0e2300ca6e3f817783f5b017b801797e38c
DarkDev <darkdev_brand@riseup.net>
2023-09-01 21:40:48 +0300

    add wrapper

--- src/lib/core/layout/entities/ui/app/startScreen/StepsList.svelte
+++ src/lib/core/layout/entities/ui/app/startScreen/StepsList.svelte
@@ -1,13 +1,13 @@
 <script>
-	import { StepElement } from '../../../../shared';
+	import { AppBlocksWrapper, StepElement } from '@coreSharedLayout';
+
 	$: stepsLength = steps.length - 1;
 
 	export let steps;
 </script>
 
-<div
-	class="mx-2 grid rounded-lg border border-pink-600 bg-black px-4 py-4 shadow-md shadow-pink-600 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:py-4"
->
+<!-- mx-2 grid rounded-lg border border-pink-600 bg-black px-4 py-4 shadow-md shadow-pink-600 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:py-4 -->
+<AppBlocksWrapper>
 	<h3 class="text-1xl mb-6 text-center font-bold text-gray-100 sm:text-4xl">
 		Kroki przygotowania do gry
 	</h3>
@@ -17,4 +17,4 @@
 		{/each}
 	</div>
 	<slot />
-</div>
+</AppBlocksWrapper>

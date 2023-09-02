ae57b50f39c5817589ee6ceb1f6bdd192a9faa95
DarkDev <darkdev_brand@riseup.net>
2023-09-02 15:00:12 +0300

    clear

--- src/lib/core/layout/shared/ui/app/AppBlocksWrapper.svelte
+++ src/lib/core/layout/shared/ui/app/AppBlocksWrapper.svelte
@@ -1,5 +1,5 @@
 <div
-	class="bg-neutal-600 mx-2 max-w-md lg:max-w-xl  h-full rounded-lg rounded-lg border-2 border border-neutral-600 p-4 text-gray-500 shadow-md shadow-md shadow-neutral-700 hover:bg-neutral-900"
+	class="bg-neutal-600 mx-2 h-full max-w-md  rounded-lg border-2  border border-neutral-600 p-4 text-gray-500 shadow-md  shadow-neutral-700 hover:bg-neutral-900 lg:max-w-xl"
 >
 	<slot />
 </div>

382fb7d1544a27bcd10a8852df368142d7d227da
DarkDev <darkdev_brand@riseup.net>
2023-09-02 14:34:55 +0300

    refactoring structure of data

--- src/lib/core/data/gameSteps.js
+++ src/lib/core/data/gameSteps.js
@@ -1,38 +1,31 @@
 const gameSteps = [
 	{
 		title: 'Lorem',
-		desc: ['Lorem'],
-		img: ''
+		desc: ['Lorem'], id: 0
 	},
 	{
 		title: 'Lorem',
-		desc: ['Lorem'],
-		img: ''
+		desc: ['Lorem'], id: 1
 	},
 	{
-		title: 'Lorem',
-		desc: ['Lorem'],
-		img: ''
+		title: 'Lorem 2',
+		desc: ['Lorem'], id: 2
 	},
 	{
-		title: 'Lorem',
-		desc: ['Lorem'],
-		img: ''
+		title: 'Lorem 3',
+		desc: ['Lorem'], id: 3
 	},
 	{
-		title: 'Lorem',
-		desc: ['Lorem'],
-		img: ''
+		title: 'Lorem 4',
+		desc: ['Lorem'], id: 4
 	},
 	{
-		title: 'Lorem',
-		desc: ['Lorem'],
-		img: ''
+		title: 'Lorem 5',
+		desc: ['Lorem'], id: 5
 	},
 	{
-		title: 'Lorem',
-		desc: ['Lorem'],
-		img: ''
+		title: 'Lorem 6',
+		desc: ['Lorem'], id: 6
 	}
 ];
 

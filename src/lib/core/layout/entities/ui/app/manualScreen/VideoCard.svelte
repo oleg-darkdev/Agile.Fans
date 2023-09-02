ad13a06321f467d07f53b09020f4344dbf488d20
DarkDev <darkdev_brand@riseup.net>
2023-09-02 14:59:56 +0300

    fix layout, add hover effects

--- src/lib/core/layout/entities/ui/app/manualScreen/VideoCard.svelte
+++ src/lib/core/layout/entities/ui/app/manualScreen/VideoCard.svelte
@@ -2,9 +2,9 @@
 	export let video;
 </script>
 
-<a href={video.link} target="_black" class="m-2 w-full  p-4 p-4 text-gray-500  lg:w-5/12 ">
-	<h3 class="mb-4 max-w-lg text-center text-3xl font-semibold text-gray-100 ">{video.title}</h3>
-	<video class="max-w-lg rounded-2xl" poster={video.poster} controls="true" playsinline muted loop>
+<a href={video.link} target="_black" class="w-full m-2 p-2 text-gray-500  max-w-md hover:bg-pink-600 rounded-lg">
+	<h3 class="mb-4 w-full text-center text-3xl font-semibold text-gray-100 ">{video.title}</h3>
+	<video class="w-full rounded-2xl" poster={video.poster} controls="true" playsinline muted loop>
 		<source src={video.link} type="video/mp4" />
 	</video>
 </a>

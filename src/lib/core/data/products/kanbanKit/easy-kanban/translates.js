fe7baf2d5c67eeb1f62fb516e84ab792b269bc36
DarkDev <darkdev_brand@riseup.net>
2023-09-02 14:31:38 +0300

    refactoring structure data for translates

---
+++ src/lib/core/data/products/kanbanKit/easy-kanban/translates.js
@@ -0,0 +1,32 @@
+const translates = [
+	{
+		lang: 'English',
+		link: ''
+	},
+	{
+		lang: 'Polish',
+		link: ''
+	},
+	{
+		lang: 'Russian',
+		link: ''
+	},
+	{
+		lang: 'Ukrainian',
+		link: ''
+	},
+	{
+		lang: 'German',
+		link: ''
+	},
+	{
+		lang: 'French',
+		link: ''
+	},
+	{
+		lang: 'Spanish',
+		link: ''
+	}
+];
+
+export default translates;

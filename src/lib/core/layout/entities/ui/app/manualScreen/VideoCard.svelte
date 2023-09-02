6dc95f9753dad7a523e478e98ab7c26995954ea7
DarkDev <darkdev_brand@riseup.net>
2023-09-02 14:38:19 +0300

    beatify

--- src/lib/core/layout/entities/ui/app/manualScreen/VideoCard.svelte
+++ src/lib/core/layout/entities/ui/app/manualScreen/VideoCard.svelte
@@ -2,13 +2,9 @@
 	export let video;
 </script>
 
-<a
-	href={video.link}
-	target="_black"
-	class="m-2 w-full  p-4 p-4 text-gray-500 shadow-md hover:bg-neutral-900  lg:w-5/12 "
->
+<a href={video.link} target="_black" class="m-2 w-full  p-4 p-4 text-gray-500  lg:w-5/12 ">
 	<h3 class="mb-4 max-w-lg text-center text-3xl font-semibold text-gray-100 ">{video.title}</h3>
-	<video class="rounded-2xl max-w-lg" poster={video.poster} controls="true" playsinline muted loop>
+	<video class="max-w-lg rounded-2xl" poster={video.poster} controls="true" playsinline muted loop>
 		<source src={video.link} type="video/mp4" />
 	</video>
 </a>

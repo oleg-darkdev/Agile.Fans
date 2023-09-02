d70b952c85dc7a45bcda30e6b562389cf9ba24fd
DarkDev <darkdev_brand@riseup.net>
2023-09-02 14:30:20 +0300

    refactoring videos data

---
+++ src/lib/core/data/products/kanbanKit/сoast-of-delay/videos.js
@@ -0,0 +1,60 @@
+
+const letsplay = {
+	title: 'Short lets play',
+	link: '/agile-fans/landing/video.mp4',
+	poster: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/fashion.jpg'
+};
+
+const webService = {
+	title: 'About online service',
+	link: '/agile-fans/landing/video.mp4',
+	poster: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/fashion.jpg'
+};
+
+
+const appVideos = {
+  welcomeToApp: {
+    title: 'Welcome video',
+    link: '/agile-fans/landing/video.mp4',
+    poster: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/fashion.jpg'
+  },
+  preparation: {
+    title: 'Welcome video',
+    link: '/agile-fans/landing/video.mp4',
+    poster: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/fashion.jpg'
+  },
+  webService,
+  appAndBoardgame: {
+    title: 'About app Boardgame',
+    link: '/agile-fans/landing/video.mp4',
+    poster: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/fashion.jpg'
+  },
+  letsplay
+};
+const	landingVideos = {
+		welcome: {
+			title: 'Short lets play',
+			link: '/agile-fans/landing/video.mp4',
+			poster: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/fashion.jpg'
+		},
+		author: {
+			title: 'Short lets play',
+			link: '/agile-fans/landing/video.mp4',
+			poster: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/fashion.jpg'
+		},
+		theory: {
+			title: 'Short lets play',
+			link: '/agile-fans/landing/video.mp4',
+			poster: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/fashion.jpg'
+		},
+		letsplay,
+		webService,
+		promo: {
+			title: 'Promo video',
+			link: '/agile-fans/landing/video.mp4',
+			poster: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/fashion.jpg'
+		}
+};
+
+
+export { landingVideos, appVideos };

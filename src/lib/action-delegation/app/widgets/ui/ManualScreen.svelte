890bd87d570bbedce363144c03d6d092fbb1b715
DarkDev <darkdev_brand@riseup.net>
2023-09-11 10:29:20 +0300

    fix height for blocks

--- src/lib/action-delegation/app/widgets/ui/ManualScreen.svelte
+++ src/lib/action-delegation/app/widgets/ui/ManualScreen.svelte
@@ -62,7 +62,7 @@
 </div>
 
 <div
-	class="bg-neutal-600 mb-6 flex h-full flex-row flex-wrap justify-around rounded-lg border-2 border border-neutral-600 p-1 text-gray-500 shadow-md  shadow-neutral-700 hover:bg-neutral-900 "
+	class="bg-neutal-600 mb-6 flex h-auto flex-row flex-wrap justify-around rounded-lg border-2 border border-neutral-600 p-1 text-gray-500 shadow-md  shadow-neutral-700 hover:bg-neutral-900 "
 >
 	{#each videoData as video}
 		<VideoCard {video} />
@@ -70,9 +70,9 @@
 </div>
 
 <section
-	class="bg-neutal-600 mx-2 h-full rounded-lg border-2  border border-neutral-600 p-4 text-gray-500 shadow-md  shadow-neutral-700 hover:bg-neutral-900 "
+	class="bg-neutal-600 mx-2 h-auto  rounded-lg border-2 mb-40 border border-neutral-600 p-4 text-gray-500 shadow-md  shadow-neutral-700 hover:bg-neutral-900 "
 >
-	<div class="container mx-auto px-6 py-10">
+	<div class="container mx-auto px-6 py-10 ">
 		<h1 class="text-2xl font-semibold text-gray-800 dark:text-white lg:text-3xl">FAQ's</h1>
 
 		{#each actionDelegation.faq.content as faqLine}

74477ab62e1593a417d2797e0e6a7a0996e23126
DarkDev <darkdev_brand@riseup.net>
2023-09-13 13:08:26 +0300

    upd data

--- src/lib/core/data/products/kanbanKit/action-delegation/appSteps.js
+++ src/lib/core/data/products/kanbanKit/action-delegation/appSteps.js
@@ -1,38 +1,68 @@
 const gameSteps = [
 	{
-		title: 'Lorem',
+		title: 'Welcome',
 		desc: ['Lorem'],
 		id: 0
 	},
 	{
-		title: 'Lorem',
+		title: 'Select the mode',
 		desc: ['Lorem'],
 		id: 1
 	},
 	{
-		title: 'Lorem 2',
+		title: 'Steps description',
 		desc: ['Lorem'],
 		id: 2
 	},
 	{
-		title: 'Lorem 3',
+		title: 'Prepare components',
 		desc: ['Lorem'],
 		id: 3
 	},
 	{
-		title: 'Lorem 4',
+		title: 'Select the roles',
 		desc: ['Lorem'],
 		id: 4
 	},
 	{
-		title: 'Lorem 5',
+		title: 'Theory: 1 delegation level ',
 		desc: ['Lorem'],
 		id: 5
 	},
 	{
-		title: 'Lorem 6',
+		title: 'Theory: 2 delegation level',
 		desc: ['Lorem'],
 		id: 6
+	},
+	{
+		title: 'Theory: 3 delegation level',
+		desc: ['Lorem'],
+		id: 7
+	},
+	{
+		title: 'Theory: 4 delegation level',
+		desc: ['Lorem'],
+		id: 8
+	},
+	{
+		title: 'Theory: 5 delegation level',
+		desc: ['Lorem'],
+		id: 9
+	},
+	{
+		title: 'Theory: 6 delegation level',
+		desc: ['Lorem'],
+		id: 10
+	},
+	{
+		title: 'Theory: 7 delegation level',
+		desc: ['Lorem'],
+		id: 11
+	},
+	{
+		title: 'Config timings',
+		desc: ['Lorem'],
+		id: 12
 	}
 ];
 
